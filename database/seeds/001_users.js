f24243b090f9441657358e36cb291c6efba8fcb8
Roenz Aberin <rmvaberin@gmail.com>
2020-01-03 11:11:52 -0800

    Added additional seeds

--- database/seeds/001_users.js
+++ database/seeds/001_users.js
@@ -20,6 +20,48 @@ exports.seed = function(knex) {
           lastName: "Ross",
           email: "test@email.com",
           password: hash
+        },
+        {
+          firstName: "Joe",
+          lastName: "Ross",
+          email: "test1@email.com",
+          password: hash
+        },
+        {
+          firstName: "Toe",
+          lastName: "Ross",
+          email: "test2@email.com",
+          password: hash
+        },
+        {
+          firstName: "Man",
+          lastName: "Ross",
+          email: "test3@email.com",
+          password: hash
+        },
+        {
+          firstName: "Laugh",
+          lastName: "Ross",
+          email: "test4@email.com",
+          password: hash
+        },
+        {
+          firstName: "New",
+          lastName: "Ross",
+          email: "test5@email.com",
+          password: hash
+        },
+        {
+          firstName: "Lorry",
+          lastName: "Ross",
+          email: "test6@email.com",
+          password: hash
+        },
+        {
+          firstName: "Feeny",
+          lastName: "Ross",
+          email: "test7@email.com",
+          password: hash
         }
       ]);
     });
--- database/seeds/002_calendar.js
+++ database/seeds/002_calendar.js
@@ -5,10 +5,15 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex("calendar").insert([
-        { users_id: 1, date: "2020-01-02", meals: 5 },
-        { users_id: 1, date: "2020-01-03", meals: 3 },
-        { users_id: 1, date: "2020-01-04", meals: 2 },
-        { users_id: 1, date: "2020-01-05", meals: 6 }
+        { users_id: 2, date: "2020-01-02", meals: 5 },
+        { users_id: 3, date: "2020-01-03", meals: 3 },
+        { users_id: 4, date: "2020-01-06", meals: 2 },
+        { users_id: 5, date: "2020-01-07", meals: 6 },
+        { users_id: 6, date: "2020-01-08", meals: 6 },
+        { users_id: 7, date: "2020-01-09", meals: 6 },
+        { users_id: 8, date: "2020-01-13", meals: 6 },
+        { users_id: 9, date: "2020-01-14", meals: 6 },
+        { users_id: 2, date: "2020-01-15", meals: 6 }
       ]);
     });
 };
